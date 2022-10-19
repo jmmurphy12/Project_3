ac429c35d833d73824e4759eee68ae02dc43565b
amadoo157 <108558585+amadoo157@users.noreply.github.com>
2022-10-18 23:14:45 -0400

    Update HeapSort.java

--- HeapSort.java
+++ HeapSort.java
@@ -1,12 +1,12 @@
 import java.io.File;
+import java.io.RandomAccessFile;
 
 // Remember to remove the package from all files!
 
 // HONOR CODE GOES HERE
 
-
 /**
- * External HeapsSort starter kit.  
+ * External HeapsSort starter kit.
  * 
  * @author {your PID}
  */
@@ -17,10 +17,26 @@ public class HeapSort {
      * 
      * @param args
      *            Command line arguments
-     * @throws Exception 
+     * @throws Exception
      */
+    @SuppressWarnings({ "unused", "rawtypes", "unchecked" })
     public static void main(String[] args) throws Exception {
-        String file = args[0];
-        BufferPool bp = new BufferPool(file);
+        String fileone = args[0];
+        // -------------------------------------
+// String numberbuffer = args[1];
+// String stats = args[2];
+// ------------------------------------
+        File file = new File(fileone);
+        RandomAccessFile access = new RandomAccessFile(file, "rw");
+        try {
+            access.read();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        BufferPool bp = new BufferPool(fileone);
+        MaxHeap max = new MaxHeap(args, 0, 0);
+        access.close();
+
     }
 }
