863a9f6fd76093d88e551a5b95dead009752aac7
amadoo157 <108558585+amadoo157@users.noreply.github.com>
2022-10-21 15:08:26 -0400

    Update MaxHeap.java

--- MaxHeap.java
+++ MaxHeap.java
@@ -8,9 +8,12 @@ import java.util.NoSuchElementException;
 
 // Many of these methods are not going to be useful for ExternalSorting...
 // Prune those methods out if you don't want to test them.
-
-public class MaxHeap<T extends Comparable<T>> {
-    private T[] heap; // Pointer to the heap array
+/**
+ * 
+ * @author amado
+ *
+ */
+public class MaxHeap {
     private int capacity; // Maximum size of the heap
     private int n; // Number of things currently in heap
     private BufferPool bpool;
@@ -24,16 +27,17 @@ public class MaxHeap<T extends Comparable<T>> {
      * @throws IOException
      * @throws NoSuchElementException
      */
-    public MaxHeap(T[] h, int heapSize, int capacity)
+    public MaxHeap(BufferPool pool, int heapSize, int capacity)
         throws NoSuchElementException,
         IOException {
-        assert capacity <= h.length : "capacity is beyond array limits";
+        assert capacity <= pool.getlength() : "capacity is beyond array limits";
         assert heapSize <= capacity : "Heap size is beyond max";
-        heap = h;
+        bpool = pool;
         n = heapSize;
         this.capacity = capacity;
         buildHeap();
     }
+
 
     // Return position for left child of pos
     /**
@@ -99,21 +103,6 @@ public class MaxHeap<T extends Comparable<T>> {
     }
 
 
-    // Insert val into heap
-    /**
-     * 
-     * @param key
-     * @throws IOException
-     * @throws NoSuchElementException
-     */
-    public void insert(T key) throws NoSuchElementException, IOException {
-        assert n < capacity : "Heap is full; cannot insert";
-        heap[n] = key;
-        n++;
-        siftUp(n - 1);
-    }
-
-
     // Organize contents of array to satisfy the heap structure
     /**
      * @throws IOException
@@ -144,7 +133,7 @@ public class MaxHeap<T extends Comparable<T>> {
             if (!isGreaterThan(child, pos)) {
                 return; // stop early
             }
-            swap(pos, child);
+            bpool.swap(pos, child);
             pos = child; // keep sifting down
         }
     }
@@ -164,7 +153,7 @@ public class MaxHeap<T extends Comparable<T>> {
             if (isGreaterThan(parent, pos)) {
                 return; // stop early
             }
-            swap(pos, parent);
+            bpool.swap(pos, parent);
             pos = parent; // keep sifting up
         }
     }
@@ -177,85 +166,14 @@ public class MaxHeap<T extends Comparable<T>> {
      * @throws IOException
      * @throws NoSuchElementException
      */
-    public T removeMax() throws NoSuchElementException, IOException {
+    public Record removeMax() throws NoSuchElementException, IOException {
         assert n > 0 : "Heap is empty; cannot remove";
         n--;
         if (n > 0) {
-            swap(0, n); // Swap maximum with last value
+            bpool.swap(0, n); // Swap maximum with last value
             siftDown(0); // Put new heap root val in correct place
         }
-        return heap[n];
-    }
-
-
-    // Remove and return element at specified position
-    /**
-     * 
-     * @param pos
-     * @return
-     * @throws IOException
-     * @throws NoSuchElementException
-     */
-    public T remove(int pos) throws NoSuchElementException, IOException {
-        assert (0 <= pos && pos < n) : "Invalid heap position";
-        n--;
-        if (n > 0) {
-            swap(pos, n); // Swap with last value
-            update(pos); // Move other value to correct position
-        }
-        return heap[n];
-    }
-
-
-    // Modify the value at the given position
-    /**
-     * 
-     * @param pos
-     * @param newVal
-     * @throws IOException
-     * @throws NoSuchElementException
-     */
-    public void modify(int pos, T newVal)
-        throws NoSuchElementException,
-        IOException {
-        assert (0 <= pos && pos < n) : "Invalid heap position";
-        heap[pos] = newVal;
-        update(pos);
-    }
-
-
-    // The value at pos has been changed, restore the heap property
-    /**
-     * 
-     * @param pos
-     * @throws IOException
-     * @throws NoSuchElementException
-     */
-    private void update(int pos) throws NoSuchElementException, IOException {
-        siftUp(pos); // priority goes up
-        siftDown(pos); // unimportant goes down
-    }
-
-
-    // swaps the elements at two positions
-    /**
-     * 
-     * @param pos1
-     * @param pos2
-     * @throws IOException
-     * @throws NoSuchElementException
-     */
-    private void swap(int pos1, int pos2)
-        throws NoSuchElementException,
-        IOException {
-// T temp = heap[pos1];
-// heap[pos1] = heap[pos2];
-// heap[pos2] = temp;
-        @SuppressWarnings("unchecked")
-        T Temp = (T)bpool.getRecord(pos1);
-        bpool.setRecord(pos1, (Record)Temp);
-        bpool.setRecord(pos2, (Record)Temp);
-
+        return bpool.getRecord(n);
     }
 
 
@@ -265,9 +183,10 @@ public class MaxHeap<T extends Comparable<T>> {
      * @param pos1
      * @param pos2
      * @return
+     * @throws IOException
      */
-    private boolean isGreaterThan(int pos1, int pos2) {
-        return heap[pos1].compareTo(heap[pos2]) > 0;
+    private boolean isGreaterThan(int pos1, int pos2) throws IOException {
+        return bpool.getRecord(pos1).compareTo(bpool.getRecord(pos2)) > 0;
     }
 
 
@@ -279,13 +198,13 @@ public class MaxHeap<T extends Comparable<T>> {
      * @throws IOException
      * @throws NoSuchElementException
      */
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    public static void heapsort(Comparable[] comp)
-        throws NoSuchElementException, IOException {
-        MaxHeap themax = new MaxHeap(comp, comp.length, comp.length);
-        for (int i = 0; i < comp.length; i++) {
+    public void Sort() throws NoSuchElementException, IOException {
+        // capacity might be wrong
+        MaxHeap themax = new MaxHeap(bpool, capacity, capacity);
+        while (n > 0) {
             themax.removeMax();
         }
 
     }
+
 }
